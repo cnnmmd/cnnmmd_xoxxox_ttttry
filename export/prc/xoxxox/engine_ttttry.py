5ac3dbf36afdfc5b59032182a68e6779861b0619
azsgit <10238943+azsgit@users.noreply.github.com>
2025-09-09 01:41:45 +0900

    add agent mode to llm handler

--- export/prc/xoxxox/engine_ttttry.py
+++ export/prc/xoxxox/engine_ttttry.py
@@ -5,12 +5,13 @@ from xoxxox.shared import Custom, LibLog
 
 class TttPrc:
 
-  def __init__(self, config="xoxxox/config_ttttry_try001", **dicprm):
+  def __init__(self, config="xoxxox/config_ttttry_cmm001", **dicprm):
     diccnf = Custom.update(config, dicprm)
     self.dictxt = {}
     self.postxt = {}
+    self.conlog = {}
 
-  def status(self, config="xoxxox/config_ttttry_try001", **dicprm):
+  def status(self, config="xoxxox/config_ttttry_cmm001", **dicprm):
     diccnf = Custom.update(config, dicprm)
     if self.dictxt != diccnf["dictxt"]:
       self.dictxt = diccnf["dictxt"]
@@ -19,12 +20,14 @@ class TttPrc:
         self.postxt[keytxt] = 0
         if self.output == "random":
           random.shuffle(self.dictxt[keytxt])
-    self.conlog = LibLog.getlog(diccnf["conlog"]) # LOG
-    self.conlog.catsys(diccnf) # LOG
+    self.expert = diccnf["expert"]
+    if not (self.expert in self.conlog):
+      self.conlog[self.expert] = LibLog.getlog(diccnf["conlog"]) # LOG
+      self.conlog[self.expert].catsys(diccnf) # LOG
 
   def infere(self, txtreq):
-    prompt = self.conlog.catreq(txtreq) # LOG
-    print("prompt[" + prompt + "]", flush=True) # DBG
+    prompt = self.conlog[self.expert].catreq(txtreq) # LOG
+    print("prompt[", prompt, "]", sep="", flush=True) # DBG
     keytxt = txtreq
     try:
       t = self.dictxt[keytxt]
@@ -34,11 +37,13 @@ class TttPrc:
       self.postxt[keytxt] = 0
       if self.output == "random":
         random.shuffle(self.dictxt[keytxt])
-    txtifr = self.dictxt[keytxt][self.postxt[keytxt]]
+    rawifr = self.dictxt[keytxt][self.postxt[keytxt]]
+    print("rawifr[", rawifr, "]", sep="", flush=True) # DBG
+    txtifr = rawifr
     print("txtifr[" + txtifr + "]", flush=True) # DBG
-    txtres, txtana = self.conlog.arrres(txtifr) # LOG
+    txtres, txtopt = self.conlog[self.expert].arrres(txtifr) # LOG
     print("txtres[" + txtres + "]", flush=True) # DBG
-    print("txtana[" + txtana + "]", flush=True) # DBG
-    self.conlog.catres(txtres) # LOG
+    print("txtana[" + txtopt + "]", flush=True) # DBG
+    self.conlog[self.expert].catres(txtres) # LOG
     self.postxt[keytxt] = self.postxt[keytxt] + 1
-    return txtres
+    return (txtres, txtopt)
