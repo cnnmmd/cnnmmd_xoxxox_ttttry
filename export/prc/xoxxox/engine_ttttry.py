b6244dd3b5b5d6089715e0f760d30457dc03438b
azsgit <10238943+azsgit@users.noreply.github.com>
2025-10-12 20:36:54 +0900

    wip

--- export/prc/xoxxox/engine_ttttry.py
+++ export/prc/xoxxox/engine_ttttry.py
@@ -7,19 +7,15 @@ class TttPrc:
 
   def __init__(self, config="xoxxox/config_ttttry_cmm001", **dicprm):
     diccnf = Custom.update(config, dicprm)
-    self.dictxt = {}
-    self.postxt = {}
+    self.postxt = 0
+    self.lsttxt = []
     self.conlog = {}
 
   def status(self, config="xoxxox/config_ttttry_cmm001", **dicprm):
     diccnf = Custom.update(config, dicprm)
-    if self.dictxt != diccnf["dictxt"]:
-      self.dictxt = diccnf["dictxt"]
-      self.output = diccnf["output"]
-      for keytxt in self.dictxt.keys():
-        self.postxt[keytxt] = 0
-        if self.output == "random":
-          random.shuffle(self.dictxt[keytxt])
+    if self.lsttxt != diccnf["lsttxt"]:
+      self.lsttxt = diccnf["lsttxt"]
+      self.postxt = 0
     self.expert = diccnf["expert"]
     if not (self.expert in self.conlog):
       self.conlog[self.expert] = LibLog.getlog(diccnf["conlog"]) # LOG
@@ -28,16 +24,9 @@ class TttPrc:
   def infere(self, txtreq):
     prompt = self.conlog[self.expert].catreq(txtreq) # LOG
     print("prompt[", prompt, "]", sep="", flush=True) # DBG
-    keytxt = txtreq
-    try:
-      t = self.dictxt[keytxt]
-    except:
-      keytxt = next(iter(self.dictxt))
-    if self.postxt[keytxt] >= len(self.dictxt[keytxt]):
-      self.postxt[keytxt] = 0
-      if self.output == "random":
-        random.shuffle(self.dictxt[keytxt])
-    rawifr = self.dictxt[keytxt][self.postxt[keytxt]]
+    if self.postxt >= len(self.lsttxt):
+      self.postxt = 0
+    rawifr = self.lsttxt[self.postxt]
     print("rawifr[", rawifr, "]", sep="", flush=True) # DBG
     txtifr = rawifr
     print("txtifr[" + txtifr + "]", flush=True) # DBG
@@ -45,5 +34,5 @@ class TttPrc:
     print("txtres[" + txtres + "]", flush=True) # DBG
     print("txtopt[" + txtopt + "]", flush=True) # DBG
     self.conlog[self.expert].catres(txtres) # LOG
-    self.postxt[keytxt] = self.postxt[keytxt] + 1
+    self.postxt = self.postxt + 1
     return (txtres, txtopt)
